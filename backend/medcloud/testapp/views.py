02cde01457a1608fcb8a204bec65193461342b06
msuleman2001 <msuleman2001@gmail.com>
2023-09-25 17:53:46 +0500

    first change

--- backend/medcloud/testapp/views.py
+++ backend/medcloud/testapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("test app is running")
+    return HttpResponse("test app is running. updated by suleman")
