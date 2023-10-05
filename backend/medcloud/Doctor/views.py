20dfb9c322ad7771ba495a6f1a98bcc6f3395150
Asad <Asad@MASADABDULLAH>
2023-10-05 15:39:15 +0500

    functions in doc app added.

--- backend/medcloud/Doctor/views.py
+++ backend/medcloud/Doctor/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import JsonResponse
 
 # Create your views here.
-def home(request):
-    msg = "Hi i am doctor"
-    
-    return HttpResponse(msg)
+
+
+
+# add doctor function
+def addDoctor(request):
+
+    return JsonResponse({'text': 'Some JSON data is rendering'})
+
+
+# get doctors
+def getDoctors(request):
+
+    return JsonResponse({'data': {"doctors":[{"name":'dr asad abudullah'}]}})
+
+# get doctor by id
+def getDoctorById(request):
+
+    return JsonResponse({'data': {"doctors":[{"name":'dr asad abudullah'}]}})
+
+
+# update doctor
+def updateDoctor(request):
+
+    return JsonResponse({'data': {"doctors":[{"name":'dr asad abudullah'}]}})
+
+# search doctor
+def searchDoctors(request):
+
+    return JsonResponse({'data': {"doctors":[{"name":'dr asad abudullah'}]}})
