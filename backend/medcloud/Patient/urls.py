5477266f79bce595791289db92a45802960768bf
muhammadmoosakaleemullah <stardark182@gmail.com>
2023-10-09 15:36:11 +0500

    patient views generated and working properly

--- backend/medcloud/Patient/urls.py
+++ backend/medcloud/Patient/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('patients', views.getPatient, name="getPatient"),
+    # Add a new patient
+    path('add-patient/', views.addPatient, name='add_patient'),
+    
+    # Get a list of all patients
+    path('get-patients/', views.getPatients, name='get_patients'),
+    
+    # Search patients based on filters (implement your logic)
+    path('search-patients/', views.searchPatients, name='search_patients'),
+    
+    # Add more URLs as needed for specific patient actions or views
 ]
--- backend/medcloud/Patient/views.py
+++ backend/medcloud/Patient/views.py
@@ -1,34 +1,91 @@
-
 from django.http import JsonResponse
+import json
+from .models import Patient
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
-# Create your views here.
+# Create a new patient in the database
 
 
-# add a new Patient in database
-
-
+@csrf_exempt
 def addPatient(request):
-    return JsonResponse({'text': 'Some JSON data is rendering'})
+    if request.method == 'POST':
+        try:
+            # Parse the incoming JSON data
+            data = json.loads(request.body)
 
-# get list of all Patient
+            # Create a new Patient object with the parsed data
+            patient = Patient(
+                patient_name=data['patient_name'],
+                patient_phone_number=data['patient_phone_number'],
+                patient_address=data['patient_address'],
+                patient_gender=data['patient_gender'],
+                patient_date_of_birth=data['patient_date_of_birth'],
+                patient_guardian=data.get('patient_guardian', None),
+                created_date_time=datetime.now(),
+                created_by_id=data['created_by_id'],
+                last_updated_date_time=datetime.now(),
+                last_updated_id=data['last_updated_id'],
+                is_enabled=data.get('is_enabled', True),
+                remarks=data.get('remarks', None)
+            )
+
+            # Save the Patient object to the database
+            patient.save()
+
+            return JsonResponse({'message': 'Patient added successfully'})
+        except KeyError:
+            return JsonResponse({'error': 'Invalid JSON data format'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+# Get a list of all patients
 
 
-def getPatient(request):
-    return JsonResponse({'data': {"Patients": [{"name": 'moosa'}]}})
+def getPatients(request):
+    try:
+        # Query all patients from the database
+        patients = Patient.objects.all()
 
-# get a single Patient record
+        # Serialize the queryset to JSON format
+        serialized_patients = [
+            {
+                'patient_id': patient.patient_id,
+                'patient_name': patient.patient_name,
+                'patient_phone_number': patient.patient_phone_number,
+                'patient_address': patient.patient_address,
+                'patient_gender': patient.patient_gender,
+                'patient_date_of_birth': patient.patient_date_of_birth,
+                'patient_guardian': patient.patient_guardian,
+                'created_date_time': patient.created_date_time,
+                'created_by_id': patient.created_by_id,
+                'last_updated_date_time': patient.last_updated_date_time,
+                'last_updated_id': patient.last_updated_id,
+                'is_enabled': patient.is_enabled,
+                'remarks': patient.remarks,
+            }
+            for patient in patients
+        ]
+
+        # Create a JSON response with the serialized data
+        response_data = {'patients': serialized_patients}
+
+        return JsonResponse(response_data)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+# Get a list of patients according to filters (you can implement this as needed)
 
 
-def getPatientById(request):
-    return JsonResponse({'data': {"Patients": [{"name": 'moosa'}]}})
+def searchPatients(request):
+    try:
+        # Get a dictionary of query parameters from the request
+        query_params = request.GET.dict()
+        # Implement your filtering logic based on query_params here
 
+        # Example filtering:
+        # patients = Patient.objects.filter(patient_name__icontains=query_params.get('name', ''))
 
-# update an existing Patient
-def updatePatient(request):
-    return JsonResponse({'data': {"Patients": [{"name": 'moosa'}]}})
-
-# get list of Patients according to filter
-
-
-def searchPatient(request):
-    return JsonResponse({'data': {"Patients": [{"name": 'moosa'}]}})
+        return JsonResponse({'message': 'Search functionality to be implemented'})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
