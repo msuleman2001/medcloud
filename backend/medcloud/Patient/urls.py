cc17c248f2838ce5a271e6aec3ac9c3503dd9aaa
muhammadmoosakaleemullah <stardark182@gmail.com>
2023-10-09 12:59:37 +0500

    test in doctor

--- backend/medcloud/Patient/urls.py
+++ backend/medcloud/Patient/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('patientlist', views.getPatient, name="getPatient"),
+    path('patients', views.getPatient, name="getPatient"),
 ]
