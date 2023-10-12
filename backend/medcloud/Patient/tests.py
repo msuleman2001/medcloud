df511f3298f8d161c71f0d48e87d65ece00cb9da
muhammadmoosakaleemullah <stardark182@gmail.com>
2023-10-12 17:06:23 +0500

    latest version with paggination

--- backend/medcloud/Patient/tests.py
+++ backend/medcloud/Patient/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Patient
 import json
@@ -9,29 +9,98 @@ class PatientViewsTest(TestCase):
 
     def test_addPatient(self):
         url = reverse('add_patient')
-        data = {
-            "patient_name": "John Doe",
-            "patient_phone_number": "123-456-7890",
-            "patient_address": "123 Main St, City",
-            "patient_gender": "Male",
-            "patient_date_of_birth": "1990-01-15",
-            "created_date_time": datetime.now().isoformat(),
-            "created_by_id": 1,
-            "last_updated_date_time": datetime.now().isoformat(),
-            "last_updated_id": 1,
-            "is_enabled": True,
-            "remarks": "This is a sample patient."
-        }
+        data = [
+            {
+                "patient_name": "John Doe",
+                "patient_phone_number": "123-456-7890",
+                "patient_address": "123 Main St, City",
+                "patient_gender": "Male",
+                "patient_date_of_birth": "1990-01-15",
+                "created_date_time": datetime.now().isoformat(),
+                "created_by_id": 1,
+                "last_updated_date_time": datetime.now().isoformat(),
+                "last_updated_id": 1,
+                "is_enabled": True,
+                "remarks": "This is a sample patient."
+            },
+            {
+                "patient_name": "Jane Smith",
+                "patient_phone_number": "987-654-3210",
+                "patient_address": "456 Elm St, Town",
+                "patient_gender": "Female",
+                "patient_date_of_birth": "1985-05-20",
+                "created_date_time": datetime.now().isoformat(),
+                "created_by_id": 2,
+                "last_updated_date_time": datetime.now().isoformat(),
+                "last_updated_id": 2,
+                "is_enabled": False,
+                "remarks": "Another sample patient."
+            }
+        ]
+
         response = self.client.post(url, json.dumps(
             data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Patient.objects.count(), 1)
+        self.assertEqual(Patient.objects.count(), 22)
 
     def test_getPatients(self):
         url = reverse('get_patients')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         # You can add more assertions to check the content of the response
+
+    def setUp(self):
+        # Create sample patient data for testing
+        for i in range(20):
+            Patient.objects.create(
+                patient_name=f"Patient {i}",
+                patient_phone_number="123-456-7890",
+                patient_address="123 Main St",
+                patient_gender="Male",
+                patient_date_of_birth="2000-01-01",
+                created_date_time="2023-10-11 12:00:00",
+                created_by_id=1,
+                last_updated_date_time="2023-10-11 12:00:00",
+                last_updated_id=1,
+                is_enabled=True,
+                remarks="Sample remarks",
+            )
+
+    def test_pagination(self):
+        # Replace 'get_patients' with your URL name
+        response = self.client.get(reverse('get_patients'))
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+
+        # Check the response structure and content
+        self.assertIn('patients', data)
+        self.assertIn('page_number', data)
+        self.assertIn('total_pages', data)
+        self.assertIn('total_records', data)
+        # Default records per page is 10
+        self.assertEqual(len(data['patients']), 10)
+        self.assertEqual(data['page_number'], 1)
+        self.assertEqual(data['total_pages'], 2)
+        self.assertEqual(data['total_records'], 20)
+
+    def test_pagination_with_custom_parameters(self):
+        # Your test code here
+
+        response = self.client.get(reverse('get_patients'), {
+            'page': 2, 'records_per_page': 5})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+
+        # Check the response content with custom parameters
+        self.assertIn('patients', data)
+        self.assertIn('page_number', data)
+        self.assertIn('total_pages', data)
+        self.assertIn('total_records', data)
+        # Custom records per page is 5
+        self.assertEqual(len(data['patients']), 5)
+        self.assertEqual(data['page_number'], 2)
+        self.assertEqual(data['total_pages'], 4)
+        self.assertEqual(data['total_records'], 20)
 
     def test_searchPatients(self):
         url = reverse('search_patients')
@@ -76,19 +145,3 @@ class PatientViewsTest(TestCase):
         # Add more assertions to check other fields
 
     # Add more test cases as needed for other views or edge cases
-
-
-# {
-#     "patient_name": "Abu Zain",
-#     "patient_phone_number": "123-456-7890",
-#     "patient_address": "123 Main St, BWP City",
-#     "patient_gender": "Male",
-#     "patient_date_of_birth": "1990-01-15",
-#     "patient_guardian": "Jane Doe",
-#     "created_date_time": "2023-10-09T12:00:00Z",
-#     "created_by_id": 1,
-#     "last_updated_date_time": "2023-10-09T12:00:00Z",
-#     "last_updated_id": 1,
-#     "is_enabled": true,
-#     "remarks": "This is a sample patient."
-# }
--- backend/medcloud/Patient/views.py
+++ backend/medcloud/Patient/views.py
@@ -3,42 +3,55 @@ import json
 from .models import Patient
 from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
-
+from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger, EmptyPage
 # Create a new patient in the database
 
 
 @csrf_exempt
 def addPatient(request):
     if request.method == 'POST':
+
         try:
-            # Parse the incoming JSON data
+            # Parse the incoming JSON data as an array of objects
             data = json.loads(request.body)
+            new_patient_id = 0
+            # Ensure that data is a list
+            if not isinstance(data, list):
+                new_patient_id = savePatient(data)
+            # Iterate over each object in the array and create a Patient object for each
+            else:
+                for item in data:
+                    # Create a new Patient object with the parsed data
+                    new_patient_id = savePatient(item)
 
-            # Create a new Patient object with the parsed data
-            patient = Patient(
-                patient_name=data['patient_name'],
-                patient_phone_number=data['patient_phone_number'],
-                patient_address=data['patient_address'],
-                patient_gender=data['patient_gender'],
-                patient_date_of_birth=data['patient_date_of_birth'],
-                patient_guardian=data.get('patient_guardian', None),
-                created_date_time=datetime.now(),
-                created_by_id=data['created_by_id'],
-                last_updated_date_time=datetime.now(),
-                last_updated_id=data['last_updated_id'],
-                is_enabled=data.get('is_enabled', True),
-                remarks=data.get('remarks', None)
-            )
-
-            # Save the Patient object to the database
-            patient.save()
-
-            return JsonResponse({'message': 'Patient added successfully'})
+            return JsonResponse({'message': 'Patients added successfully', 'new_patient_id': new_patient_id})
         except KeyError:
-            return JsonResponse({'error': 'Invalid JSON data format'}, status=400)
+            return JsonResponse({'error': 'Invalid JSON data format in one or more objects'}, status=400)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
+
+def savePatient(patient_item):
+    t = type(patient_item)
+    patient = Patient(
+        patient_name=patient_item['patient_name'],
+        patient_phone_number=patient_item['patient_phone_number'],
+        patient_address=patient_item['patient_address'],
+        patient_gender=patient_item['patient_gender'],
+        patient_date_of_birth=patient_item['patient_date_of_birth'],
+        patient_guardian=patient_item.get('patient_guardian', None),
+        created_date_time=datetime.now(),
+        created_by_id=patient_item['created_by_id'],
+        last_updated_date_time=datetime.now(),
+        last_updated_id=patient_item['last_updated_id'],
+        is_enabled=patient_item.get('is_enabled', True),
+        remarks=patient_item.get('remarks', None)
+    )
+
+    # Save the Patient object to the database
+    patient.save()
+    return patient.patient_id
 # Get a list of all patients
 
 
@@ -75,6 +88,59 @@ def getPatients(request):
         return JsonResponse({'error': str(e)}, status=500)
 
 # Get a list of patients according to filters (you can implement this as needed)
+
+
+def getPatients(request):
+    try:
+        # Get parameters from the request
+        # Default to page 1 if not specified
+        page_number = int(request.GET.get('page', 1))
+        # Default to 10 records per page
+        records_per_page = int(request.GET.get('records_per_page', 10))
+
+        # Query all patients from the database
+        patients = Patient.objects.all()
+
+        # Create a Paginator object
+        paginator = Paginator(patients, records_per_page)
+
+        # Get the specified page
+        try:
+            current_page = paginator.page(page_number)
+        except EmptyPage:
+            return JsonResponse({'error': 'Invalid page number'}, status=400)
+
+        # Serialize the paginated data to JSON format
+        serialized_patients = [
+            {
+                'patient_id': patient.patient_id,
+                'patient_name': patient.patient_name,
+                'patient_phone_number': patient.patient_phone_number,
+                'patient_address': patient.patient_address,
+                'patient_gender': patient.patient_gender,
+                'patient_date_of_birth': patient.patient_date_of_birth,
+                'patient_guardian': patient.patient_guardian,
+                'created_date_time': patient.created_date_time,
+                'created_by_id': patient.created_by_id,
+                'last_updated_date_time': patient.last_updated_date_time,
+                'last_updated_id': patient.last_updated_id,
+                'is_enabled': patient.is_enabled,
+                'remarks': patient.remarks,
+            }
+            for patient in current_page
+        ]
+
+        # Create a JSON response with the serialized data
+        response_data = {
+            'patients': serialized_patients,
+            'page_number': current_page.number,
+            'total_pages': paginator.num_pages,
+            'total_records': paginator.count,
+        }
+
+        return JsonResponse(response_data)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 
 def searchPatients(request):
