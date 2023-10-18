6548033654d689e1564b92ecfa1f5fd72153291f
Asad <Asad@MASADABDULLAH>
2023-10-18 13:17:48 +0500

    to django rest framework

--- backend/medcloud/Doctor/tests.py
+++ backend/medcloud/Doctor/tests.py
@@ -1,158 +1,185 @@
+# from django.test import TestCase
+# from django.urls import reverse
+# from django.http import JsonResponse
+# from .models import Doctor
+# import json
+# from django.contrib.auth.models import User
+
+# class DoctorViewTestCase(TestCase):
+#     def test_add_doctor(self):
+#         # Define the data for the new doctor to be added
+#         new_doctor_data = {
+#             'name': 'New Doctor',
+#             'email': 'newdoctor@example.com',
+#             'phone': '987-654-3210',
+#             'license': '54321',  # Corrected key to 'license'
+#             'speciality': 'Neurology',
+#             'start_year': 2010,
+#             'clinic_address': '789 Elm St',
+#             'added_by_id': 3,
+#             'remarks': 'Test remarks for New Doctor'
+#         }
+
+#         # Create a JSON string from the new doctor data
+#         new_doctor_data_json = json.dumps(new_doctor_data)
+
+#         # Test the addDoctor view by making a POST request
+#         url = reverse('doctors')  # Assuming you have defined a URL pattern with the name 'doctors'
+#         response = self.client.post(url, data=new_doctor_data_json, content_type='application/json')
+
+#         # Check if the response status code is 200 (OK)
+#         self.assertEqual(response.status_code, 200)
+
+#         # Check if the response contains the expected message
+#         expected_response = {'message': 'Doctor added successfully'}
+#         self.assertEqual(response.json(), expected_response)
+
+#         # Verify that the new doctor has been added to the database
+#         new_doctor = Doctor.objects.get(name='New Doctor')
+#         self.assertIsNotNone(new_doctor)
+#         # Add more assertions to verify the data of the new doctor if needed
+    
+#     def setUp(self):
+#         # Create a user (assuming 'User' is the model for the added_by_id field)
+#         user = User.objects.create(username="testuser")
+
+#         # Create some sample doctors in the database for testing
+#         Doctor.objects.create(
+#             name="Dr. John Doe",
+#             speciality="Cardiology",
+#             start_year=2005,
+#             added_by_id=user.id  # Provide the ID of the user as the added_by_id
+#         )
+#         Doctor.objects.create(
+#             name="Dr. Jane Smith",
+#             speciality="Dermatology",
+#             start_year=2008,
+#             added_by_id=user.id
+#         )
+#         Doctor.objects.create(
+#             name="Dr. Michael Brown",
+#             speciality="Orthopedics",
+#             start_year=2010,
+#             added_by_id=user.id
+#         )
+#         # Add more sample doctors as needed
+
+#     def test_get_doctors_with_filter(self):
+#         # Define the filter criteria
+#         filters = {'name': 'John', 'speciality': 'Cardiology'}
+
+#         # Generate the URL for your getDoctors view, including filter criteria
+#         url = reverse('getDoctors')  # Replace 'get_doctors' with your actual URL name
+
+#         # Make a GET request with the filter criteria
+#         response = self.client.get(url, data=filters)
+
+#         # Check if the response status code is 200 (OK)
+#         self.assertEqual(response.status_code, 200)
+
+#         # Check if the filtered doctor's name is in the response content
+#         self.assertContains(response, 'Dr. John Doe')
+
+#         # You can add more assertions based on the expected results
+
+#     def test_get_doctors_without_filter(self):
+#         # Generate the URL for your getDoctors view without filter criteria
+#         url = reverse('getDoctors')  # Replace 'get_doctors' with your actual URL name
+
+#         # Make a GET request without filter criteria
+#         response = self.client.get(url)
+
+#         # Check if the response status code is 200 (OK)
+#         self.assertEqual(response.status_code, 200)
+
+    
+    
+    
+
+# # {
+# #       "email": "abc10@gmail.com",
+# #       "name": "Asad",
+# #       "phone": "+923440059960",
+# #       "license": "abc123",
+# #       "speciality": "Ophthalmologist",
+# #       "start_year": "2018",
+# #       "clinic_address": "BWP",
+# #       "country": "Pakistan",
+# #       "added_by_id": "1",
+# #       "remarks": "Nothing"
+# #     },
+# #     {
+# #       "email": "abc11@gmail.com",
+# #       "name": "Asad",
+# #       "phone": "+923440059961",
+# #       "license": "abc123",
+# #       "speciality": "Ophthalmologist",
+# #       "start_year": "2018",
+# #       "clinic_address": "BWP",
+# #       "country": "Pakistan",
+# #       "added_by_id": "1",
+# #       "remarks": "Nothing"
+# #     },
+# #     {
+# #       "email": "abc12@gmail.com",
+# #       "name": "Asad",
+# #       "phone": "+923440059962",
+# #       "license": "abc123",
+# #       "speciality": "Ophthalmologist",
+# #       "start_year": "2018",
+# #       "clinic_address": "BWP",
+# #       "country": "Pakistan",
+# #       "added_by_id": "1",
+# #       "remarks": "Nothing"
+# #     },
+# #     {
+# #       "email": "abc13@gmail.com",
+# #       "name": "Asad",
+# #       "phone": "+923440059963",
+# #       "license": "abc123",
+# #       "speciality": "Ophthalmologist",
+# #       "start_year": "2018",
+# #       "clinic_address": "BWP",
+# #       "country": "Pakistan",
+# #       "added_by_id": "1",
+# #       "remarks": "Nothing"
+# #     },
+# #     {
+# #       "email": "abc14@gmail.com",
+# #       "name": "Asad",
+# #       "phone": "+923440059964",
+# #       "license": "abc123",
+# #       "speciality": "Ophthalmologist",
+# #       "start_year": "2018",
+# #       "clinic_address": "BWP",
+# #       "country": "Pakistan",
+# #       "added_by_id": "1",
+# #       "remarks": "Nothing"
+# #     }
+
+from rest_framework.test import APIClient
 from django.test import TestCase
-from django.urls import reverse
-from django.http import JsonResponse
 from .models import Doctor
-import json
-from django.contrib.auth.models import User
 
-class DoctorViewTestCase(TestCase):
-    def test_add_doctor(self):
-        # Define the data for the new doctor to be added
-        new_doctor_data = {
-            'name': 'New Doctor',
-            'email': 'newdoctor@example.com',
-            'phone': '987-654-3210',
-            'license': '54321',  # Corrected key to 'license'
-            'speciality': 'Neurology',
-            'start_year': 2010,
-            'clinic_address': '789 Elm St',
-            'added_by_id': 3,
-            'remarks': 'Test remarks for New Doctor'
-        }
-
-        # Create a JSON string from the new doctor data
-        new_doctor_data_json = json.dumps(new_doctor_data)
-
-        # Test the addDoctor view by making a POST request
-        url = reverse('doctors')  # Assuming you have defined a URL pattern with the name 'doctors'
-        response = self.client.post(url, data=new_doctor_data_json, content_type='application/json')
-
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-
-        # Check if the response contains the expected message
-        expected_response = {'message': 'Doctor added successfully'}
-        self.assertEqual(response.json(), expected_response)
-
-        # Verify that the new doctor has been added to the database
-        new_doctor = Doctor.objects.get(name='New Doctor')
-        self.assertIsNotNone(new_doctor)
-        # Add more assertions to verify the data of the new doctor if needed
-    
+class DoctorTests(TestCase):
     def setUp(self):
-        # Create a user (assuming 'User' is the model for the added_by_id field)
-        user = User.objects.create(username="testuser")
+        self.client = APIClient()
+        self.doctor_data = {
+            'email': 'doctor@example.com',
+            'name': 'John Doe',
+            # Add other fields as needed
+        }
+        self.doctor = Doctor.objects.create(**self.doctor_data)
 
-        # Create some sample doctors in the database for testing
-        Doctor.objects.create(
-            name="Dr. John Doe",
-            speciality="Cardiology",
-            start_year=2005,
-            added_by_id=user.id  # Provide the ID of the user as the added_by_id
-        )
-        Doctor.objects.create(
-            name="Dr. Jane Smith",
-            speciality="Dermatology",
-            start_year=2008,
-            added_by_id=user.id
-        )
-        Doctor.objects.create(
-            name="Dr. Michael Brown",
-            speciality="Orthopedics",
-            start_year=2010,
-            added_by_id=user.id
-        )
-        # Add more sample doctors as needed
+    def test_create_doctor(self):
+        response = self.client.post('/doctors/', self.doctor_data, format='json')
+        self.assertEqual(response.status_code, 201)
 
-    def test_get_doctors_with_filter(self):
-        # Define the filter criteria
-        filters = {'name': 'John', 'speciality': 'Cardiology'}
-
-        # Generate the URL for your getDoctors view, including filter criteria
-        url = reverse('getDoctors')  # Replace 'get_doctors' with your actual URL name
-
-        # Make a GET request with the filter criteria
-        response = self.client.get(url, data=filters)
-
-        # Check if the response status code is 200 (OK)
+    def test_get_doctor(self):
+        response = self.client.get(f'/doctors/detail/?query={self.doctor.id}')
         self.assertEqual(response.status_code, 200)
 
-        # Check if the filtered doctor's name is in the response content
-        self.assertContains(response, 'Dr. John Doe')
-
-        # You can add more assertions based on the expected results
-
-    def test_get_doctors_without_filter(self):
-        # Generate the URL for your getDoctors view without filter criteria
-        url = reverse('getDoctors')  # Replace 'get_doctors' with your actual URL name
-
-        # Make a GET request without filter criteria
-        response = self.client.get(url)
-
-        # Check if the response status code is 200 (OK)
+    def test_update_doctor(self):
+        updated_data = {'name': 'Updated Name'}
+        response = self.client.put(f'/doctors/detail/?query={self.doctor.id}', updated_data, format='json')
         self.assertEqual(response.status_code, 200)
-
-    
-    
-    
-
-# {
-#       "email": "abc10@gmail.com",
-#       "name": "Asad",
-#       "phone": "+923440059960",
-#       "license": "abc123",
-#       "speciality": "Ophthalmologist",
-#       "start_year": "2018",
-#       "clinic_address": "BWP",
-#       "country": "Pakistan",
-#       "added_by_id": "1",
-#       "remarks": "Nothing"
-#     },
-#     {
-#       "email": "abc11@gmail.com",
-#       "name": "Asad",
-#       "phone": "+923440059961",
-#       "license": "abc123",
-#       "speciality": "Ophthalmologist",
-#       "start_year": "2018",
-#       "clinic_address": "BWP",
-#       "country": "Pakistan",
-#       "added_by_id": "1",
-#       "remarks": "Nothing"
-#     },
-#     {
-#       "email": "abc12@gmail.com",
-#       "name": "Asad",
-#       "phone": "+923440059962",
-#       "license": "abc123",
-#       "speciality": "Ophthalmologist",
-#       "start_year": "2018",
-#       "clinic_address": "BWP",
-#       "country": "Pakistan",
-#       "added_by_id": "1",
-#       "remarks": "Nothing"
-#     },
-#     {
-#       "email": "abc13@gmail.com",
-#       "name": "Asad",
-#       "phone": "+923440059963",
-#       "license": "abc123",
-#       "speciality": "Ophthalmologist",
-#       "start_year": "2018",
-#       "clinic_address": "BWP",
-#       "country": "Pakistan",
-#       "added_by_id": "1",
-#       "remarks": "Nothing"
-#     },
-#     {
-#       "email": "abc14@gmail.com",
-#       "name": "Asad",
-#       "phone": "+923440059964",
-#       "license": "abc123",
-#       "speciality": "Ophthalmologist",
-#       "start_year": "2018",
-#       "clinic_address": "BWP",
-#       "country": "Pakistan",
-#       "added_by_id": "1",
-#       "remarks": "Nothing"
-#     }
--- backend/medcloud/Doctor/views.py
+++ backend/medcloud/Doctor/views.py
@@ -1,123 +1,169 @@
-from django.http import JsonResponse
-import json
-from .models import Doctor
-from django.views.decorators.csrf import csrf_exempt
-from datetime import datetime
-from django.core import serializers
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.paginator import Paginator
-# Create your views here.
-# add a new doctor in database
-@csrf_exempt
-def addDoctor(request):
-    if request.method == 'POST':
-        try:
-            # Parse the incoming JSON data
-            data = json.loads(request.body)
+# from django.http import JsonResponse
+# import json
+# from .models import Doctor
+# from django.views.decorators.csrf import csrf_exempt
+# from datetime import datetime
+# from django.core import serializers
+# from django.core.exceptions import ObjectDoesNotExist
+# from django.core.paginator import Paginator
+# # Create your views here.
+# # add a new doctor in database
+# @csrf_exempt
+# def addDoctor(request):
+#     if request.method == 'POST':
+#         try:
+#             # Parse the incoming JSON data
+#             data = json.loads(request.body)
             
-            # Create a new Doctor object with the parsed data
-            doctor = Doctor(
-                email=data['email'],
-                name=data['name'],
-                phone=data['phone'],
-                license_no=data['license'],  # Corrected key to 'license'
-                speciality=data['speciality'],
-                start_year=data['start_year'],
-                clinic_address=data['clinic_address'],
-                country='pakistan',  # Corrected key to 'country'
-                added_by_id=data['added_by_id'],
-                added_datetime=datetime.now(),
-                last_update_date_time=datetime.now(),
-                remarks=data['remarks'],
-                is_enabled=False,
+#             # Create a new Doctor object with the parsed data
+#             doctor = Doctor(
+#                 email=data['email'],
+#                 name=data['name'],
+#                 phone=data['phone'],
+#                 license_no=data['license'],  # Corrected key to 'license'
+#                 speciality=data['speciality'],
+#                 start_year=data['start_year'],
+#                 clinic_address=data['clinic_address'],
+#                 country='pakistan',  # Corrected key to 'country'
+#                 added_by_id=data['added_by_id'],
+#                 added_datetime=datetime.now(),
+#                 last_update_date_time=datetime.now(),
+#                 remarks=data['remarks'],
+#                 is_enabled=False,
                 
-            )
+#             )
             
-            # Save the Doctor object to the database
-            doctor.save()
+#             # Save the Doctor object to the database
+#             doctor.save()
             
-            return JsonResponse({'message': 'Doctor added successfully'})
-        except KeyError:
-            return JsonResponse({'error': 'Invalid JSON data format'}, status=400)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
+#             return JsonResponse({'message': 'Doctor added successfully'})
+#         except KeyError:
+#             return JsonResponse({'error': 'Invalid JSON data format'}, status=400)
+#     else:
+#         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 
 
 
-# get list of all doctors
-@csrf_exempt
-def getDoctors(request):
-    try:
-        # Get query parameters for pagination
-        page_number = request.GET.get('page', 1)
-        per_page = request.GET.get('per_page', 10)
+# # get list of all doctors
+# @csrf_exempt
+# def getDoctors(request):
+#     try:
+#         # Get query parameters for pagination
+#         page_number = request.GET.get('page', 1)
+#         per_page = request.GET.get('per_page', 10)
 
-        # Build a dictionary to hold filter criteria
-        filters = {}
-        for param, value in request.GET.items():
-            if param in ['page', 'per_page']:
-                continue  # Skip pagination parameters
-            filters[f'{param}__icontains'] = value  # Assume case-insensitive substring matching
+#         # Build a dictionary to hold filter criteria
+#         filters = {}
+#         for param, value in request.GET.items():
+#             if param in ['page', 'per_page']:
+#                 continue  # Skip pagination parameters
+#             filters[f'{param}__icontains'] = value  # Assume case-insensitive substring matching
 
-        # Query doctors from the database with the dynamic filters
-        doctors = Doctor.objects.filter(**filters)
+#         # Query doctors from the database with the dynamic filters
+#         doctors = Doctor.objects.filter(**filters)
 
-        # Create a paginator with the filtered queryset
-        paginator = Paginator(doctors, per_page)
+#         # Create a paginator with the filtered queryset
+#         paginator = Paginator(doctors, per_page)
 
-        # Get the specified page
-        page = paginator.page(page_number)
+#         # Get the specified page
+#         page = paginator.page(page_number)
 
-        # Serialize the queryset to JSON format
-        serialized_doctors = [
-            {
-                'id': doctor.id,
-                'name': doctor.name,
-                # Add other fields as needed
-            }
-            for doctor in page
-        ]
+#         # Serialize the queryset to JSON format
+#         serialized_doctors = [
+#             {
+#                 'id': doctor.id,
+#                 'name': doctor.name,
+#                 # Add other fields as needed
+#             }
+#             for doctor in page
+#         ]
 
-        # Create a JSON response with the serialized data
-        response_data = {
-            'doctors': serialized_doctors,
-            'total_pages': paginator.num_pages,
-            'current_page': page_number,
-            'total_records': paginator.count,
-            'records_per_page': per_page,
-        }
+#         # Create a JSON response with the serialized data
+#         response_data = {
+#             'doctors': serialized_doctors,
+#             'total_pages': paginator.num_pages,
+#             'current_page': page_number,
+#             'total_records': paginator.count,
+#             'records_per_page': per_page,
+#         }
 
-        return JsonResponse(response_data)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+#         return JsonResponse(response_data)
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=500)
 
-# get list of doctors according to filter
+# # get list of doctors according to filter
 
-@csrf_exempt    
-def updateDoctor(request):
-    try:
-        # Get the 'query' parameter from the request
-        query = request.GET.get('query')
+# @csrf_exempt    
+# def updateDoctor(request):
+#     try:
+#         # Get the 'query' parameter from the request
+#         query = request.GET.get('query')
 
-        # Parse the JSON data from the request body
-        data = json.loads(request.body)
+#         # Parse the JSON data from the request body
+#         data = json.loads(request.body)
 
-        # Query the doctor based on the 'query' parameter
-        doctor = Doctor.objects.get(pk=query)
+#         # Query the doctor based on the 'query' parameter
+#         doctor = Doctor.objects.get(pk=query)
 
-        # Update the doctor's information with the provided data
-        for field, value in data.items():
-            setattr(doctor, field, value)
+#         # Update the doctor's information with the provided data
+#         for field, value in data.items():
+#             setattr(doctor, field, value)
 
-        # Save the updated doctor object to the database
-        doctor.save()
+#         # Save the updated doctor object to the database
+#         doctor.save()
 
-        return JsonResponse({'message': 'Doctor updated successfully'})
-    except Doctor.DoesNotExist:
-        return JsonResponse({'error': 'Doctor not found'}, status=404)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+#         return JsonResponse({'message': 'Doctor updated successfully'})
+#     except Doctor.DoesNotExist:
+#         return JsonResponse({'error': 'Doctor not found'}, status=404)
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=500)
+from .models import Doctor
+from .serializer import DoctorSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class addDoctor(APIView):
+    def post(self, request):
+        serializer = DoctorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Doctor added successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class getDoctors(APIView):
+    def get(self, request):
+        query = request.query_params.get('query')
+        try:
+            doctor = Doctor.objects.get(pk=query)
+            serializer = DoctorSerializer(doctor)
+            return Response(serializer.data)
+        except Doctor.DoesNotExist:
+            return Response({'error': 'Doctor not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # def updateDoctor(self, request):
+    #     query = request.query_params.get('query')
+    #     try:
+    #         doctor = Doctor.objects.get(pk=query)
+    #         serializer = DoctorSerializer(doctor, data=request.data)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return Response({'message': 'Doctor updated successfully'})
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     except Doctor.DoesNotExist:
+    #         return Response({'error': 'Doctor not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
-
+class updateDoctor(APIView):
+    def put(self, request):
+        query = request.query_params.get('query')
+        try:
+            doctor = Doctor.objects.get(pk=query)
+            serializer = DoctorSerializer(doctor, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Doctor updated successfully'})
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Doctor.DoesNotExist:
+            return Response({'error': 'Doctor not found'}, status=status.HTTP_404_NOT_FOUND)
